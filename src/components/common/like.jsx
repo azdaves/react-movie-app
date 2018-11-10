fb49d4cee976600c0054534aa4298483bbf4a3e3
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-11-10 11:33:52 -0700

    added like function

---
+++ src/components/common/like.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+// input boolean liked
+// output raise onclick event
+
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
+
+export default Like;
